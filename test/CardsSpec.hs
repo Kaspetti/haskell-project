86a139f05a81e5a6cb1bedd28d17d8c27556a46e
kaspar moberg <kaspeti123@gmail.com>
2023-04-20 23:17:00 +0200

    Added tests for Cards module

---
+++ test/CardsSpec.hs
@@ -0,0 +1,17 @@
+module CardsSpec (spec) where
+
+import Test.Hspec
+
+import Cards
+import Data.List (nub)
+
+spec :: Spec
+spec = do
+  describe "createDeck" $ do
+    it "deck has 52 cards" $ do
+      length createDeck `shouldBe` 52
+    it "deck has unique cards" $ do
+      length (nub createDeck) `shouldBe` 52
+    it "shuffled deck has same cards as unshuffled deck" $ do
+      shuffledDeck <- shuffleDeck createDeck
+      shuffledDeck `shouldMatchList` createDeck
